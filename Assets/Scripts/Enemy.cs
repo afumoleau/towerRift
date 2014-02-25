5399fb4362641295d844c3593a00d548d5b7cf60
eolhing <eolhing@gmail.com>
2014-02-25 13:41:37 +0100

    Add enemies and EnemyManager to spawn them

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+	public Transform target;
+	private NavMeshAgent agent;
+
+	void Start ()
+	{
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination(target.position);
+	}
+	
+	void Update ()
+	{
+	}
+}
---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour
+{
+	public Transform source;
+	public Transform destination;
+	public float spawnDelay;
+	public int leftToSpawn;
+
+	public Transform spawn;
+
+	private float clock = 0f;
+
+	void Start ()
+	{
+	}
+	
+	void Update ()
+	{
+		clock += Time.deltaTime;
+		if(clock > spawnDelay && leftToSpawn > 0)
+		{
+			Transform newSpawn = Instantiate(spawn) as Transform;
+			newSpawn.parent = transform;
+			newSpawn.position = source.position;
+			newSpawn.GetComponent<NavMeshAgent>().SetDestination(destination.position);
+
+			leftToSpawn--;
+			clock = 0;
+		}
+	}
+}
