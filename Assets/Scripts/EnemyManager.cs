ba7f3d6eefaaccf5a9896803eb35b49e2f1c1387
VicKing <victorfrancois@free.fr>
2014-03-04 20:06:17 +0100

    Spawn Random

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -3,30 +3,58 @@ using System.Collections;
 
 public class EnemyManager : MonoBehaviour
 {
-	public Transform source;
+	public Transform source1;
+	public Transform source2;
+	public Transform source3;
+	public Transform source4;
 	public Transform destination;
 	public float spawnDelay;
 	public int leftToSpawn;
 	public Transform spawn;
 
+	private Transform source;
 	private float clock = 0f;
 
 	void Start ()
 	{
+		spawnRandom ();
 	}
 	
 	void Update ()
 	{
 		clock += Time.deltaTime;
-		if (clock > spawnDelay && leftToSpawn > 0) {
-			Transform newSpawn = Instantiate (spawn) as Transform;
-			newSpawn.parent = transform;
-			newSpawn.position = source.position;
-			newSpawn.GetComponent<NavMeshAgent> ().SetDestination (destination.position);
+		if (clock > spawnDelay) {
+			if (leftToSpawn > 0) {
+				Transform newSpawn = Instantiate (spawn) as Transform;
+				newSpawn.parent = transform;
+				newSpawn.position = source.position;
+				newSpawn.GetComponent<NavMeshAgent> ().SetDestination (destination.position);
+				leftToSpawn--;
+				clock = 0;
 
-			leftToSpawn--;
-			clock = 0;
+				if (leftToSpawn == 0) { // A vérifier avec plusieurs vagues d'ennemies.
+					spawnRandom();
+				}
+			}
 		}
 
+	}
+
+	void spawnRandom () {
+		int random = Random.Range (0, 4);
+		switch (random) { 
+		case 0:
+			source = source1;
+			break;
+		case 1: 
+			source = source2;
+			break;
+		case 2: 
+			source = source3;
+			break;
+		case 3: 
+			source = source4; 
+			break;
+		}
 	}
 }
