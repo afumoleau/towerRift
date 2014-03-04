b8bb419213f6ac33b125db6ce01840d8f3d80940
VicKing <victorfrancois@free.fr>
2014-03-04 19:09:08 +0100

    amelioration tactician menu

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,12 +5,19 @@ public class Enemy : MonoBehaviour
 {
 	public int hitPoints = 5;
 
+	public int maxHealth = 5;
+	public int curHealth = 100;
+	
+	public float healthBarLength;
+
 	void Start ()
 	{
+		healthBarLength = Screen.width / 6;
 	}
 	
 	void Update ()
 	{
+		AddjustCurrentHealth(0);   
 	}
 
 	public void hit()
@@ -18,5 +25,23 @@ public class Enemy : MonoBehaviour
 		hitPoints--;
 		if(hitPoints <= 0)
 			Destroy(gameObject);
+	}
+
+	void OnGUI() {
+		Vector2 targetPos;
+		targetPos = Camera.main.WorldToScreenPoint (transform.position);
+		GUI.Box(new Rect(targetPos.x, Screen.height - targetPos.y, 40, 30), curHealth + "/" + maxHealth);
+	}
+
+	public void AddjustCurrentHealth(int adj) {
+		curHealth += adj;
+	
+		if (curHealth < 0)
+			curHealth = 0;
+
+		if (curHealth > maxHealth)
+			curHealth = maxHealth;
+		
+		healthBarLength = (Screen.width / 6) * (curHealth /(float)maxHealth);
 	}
 }
--- Assets/Scripts/TacticienMenu.cs
+++ Assets/Scripts/TacticienMenu.cs
@@ -20,13 +20,13 @@ public class TacticienMenu : MonoBehaviour {
 		int menuSizeY = Screen.height;
 		int menuPosX = 10;
 		int menuPosY = 10;//Screen.height/2 - menuSizeY/2;
-		int sizeButtonX = 250;
+		int sizeButtonX = menuSizeX/4 - 20;
 		int sizeButtonY = 30;
 		Rect mainMenu = new Rect(menuPosX, menuPosY, menuSizeX, menuSizeY);
 		
 		GUI.BeginGroup (mainMenu, "");
-		GUI.Box (new Rect(0, 0, menuSizeX/4 - 10, menuSizeY - 20), "Actions");
-		
+		GUI.Box (new Rect(0, 0, menuSizeX/4, menuSizeY - 20), "Actions");
+	
 		if (GUI.Button (new Rect (10,30, sizeButtonX, sizeButtonY), "Action 1")) {
 
 		}
@@ -40,15 +40,15 @@ public class TacticienMenu : MonoBehaviour {
 			
 		}
 
-		GUI.Box (new Rect ((menuSizeX / 4 * 3) - 10, 0, menuSizeX / 4 - 10, menuSizeY / 4 * 3 - 20), "Menu");
+		GUI.Box (new Rect ((menuSizeX / 4 * 3) - 20, 0, menuSizeX / 4, menuSizeY / 4 * 3 - 20), "Menu");
 
-		if (GUI.Button (new Rect (menuSizeX / 4 * 3, 30, sizeButtonX, sizeButtonY), "Pause / Reprendre")) {
+		if (GUI.Button (new Rect (menuSizeX / 4 * 3 - 10, 30, sizeButtonX, sizeButtonY), "Pause / Reprendre")) {
 			/*if (stop == false)
 				stop = true;
 			else
 				stop = false;*/
 		}
-		if (GUI.Button (new Rect (menuSizeX / 4 * 3, 70, sizeButtonX, sizeButtonY), "Quitter")) {
+		if (GUI.Button (new Rect (menuSizeX / 4 * 3 - 10, 70, sizeButtonX, sizeButtonY), "Quitter")) {
 			
 		}
 		GUI.EndGroup ();
